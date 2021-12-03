319864d3c43792fc9e13b32452e819a37e74fd9b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-12-03 17:53:12 -0300

    Create ft_argument_s.c

---
+++ printf/ft_argument_s.c
@@ -0,0 +1,12 @@
+#include "ft_printf.h"
+
+int ft_argument_s(char *str)
+{
+	if (str == NULL)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	ft_putstr_fd(str, 1);
+	return (ft_strlen(str));
+}
