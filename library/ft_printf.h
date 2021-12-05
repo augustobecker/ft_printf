06fe4d7bcf10d2e8ecb9eb4483938c46764af9ce
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-12-05 19:56:37 -0300

    Update ft_printf.h

--- library/ft_printf.h
+++ library/ft_printf.h
@@ -3,16 +3,16 @@
 # include "libft.h"
 # include <stdarg.h>
 
-int	 	ft_printf(const char *str, ...);
-int		ft_argument_c(char c);
-int 	ft_arguments_d_i(int c);
-int 	ft_argument_p(unsigned long int address);
-int 	ft_argument_percent(void);
-int 	ft_argument_s(char *str);
-int 	ft_argument_u(unsigned int nbr);
-int		ft_arguments_x(unsigned long int nbr, char type);
-int		ft_is_argument(char a);
-int		ft_nbrlen(long int nbr);
-void	ft_free_ptr(char **ptr);
+int   ft_printf(const char *str, ...);
+int   ft_argument_c(char c);
+int   ft_arguments_d_i(int c);
+int   ft_argument_p(unsigned long int address);
+int   ft_argument_percent(void);
+int   ft_argument_s(char *str);
+int   ft_argument_u(unsigned int nbr);
+int   ft_arguments_x(unsigned int nbr, char type);
+int   ft_is_argument(char a);
+int   ft_nbrlen(long int nbr);
+void  ft_free_ptr(char **ptr);
 
 #endif
