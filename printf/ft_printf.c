fecb15ecfeb229c66ad8ed2137323eff22cb6244
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-12-14 10:24:52 -0300

    Update ft_printf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -20,13 +20,13 @@ int ft_printf(const char *str, ...)
 			if (str[i + 1] == 's')
 				bytes_writed += ft_argument_s(va_arg(argptr, char *));
 			if (str[i + 1] == 'p')
-				bytes_writed += ft_argument_p(va_arg(argptr, int));
+				bytes_writed += ft_argument_p(va_arg(argptr, unsigned long long int));
 			if (str[i + 1] == 'd' || str[i + 1] == 'i')
 				bytes_writed += ft_arguments_d_i(va_arg(argptr, int));
 			if (str[i + 1] == 'u')
 				bytes_writed += ft_argument_u(va_arg(argptr, int));
 			if (str[i + 1] == 'x' || str[i + 1] == 'X')
-				bytes_writed += ft_arguments_x(va_arg(argptr, int), str[i + 1]);
+				bytes_writed += ft_arguments_x(va_arg(argptr, unsigned int), str[i + 1]);
 			i++;
 		}
 		else
