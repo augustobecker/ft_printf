92fa875898c415f9f02c3bc893f749e1700f4c6a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-12-28 17:25:00 -0300

    Update ft_arguments_d_i.c

--- printf/ft_arguments_d_i.c
+++ printf/ft_arguments_d_i.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_arguments_d_i.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 19:05:57 by acesar-l          #+#    #+#             */
+/*   Updated: 2021/12/14 19:05:58 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-static void ft_putnbr(int nb)
+static void	ft_putnbr(int nb)
 {
 	int	soma;
 
@@ -24,7 +36,7 @@ static void ft_putnbr(int nb)
 	}	
 }
 
-int ft_arguments_d_i(int c)
+int	ft_arguments_d_i(int c)
 {
 	ft_putnbr(c);
 	return (ft_nbrlen(c));
