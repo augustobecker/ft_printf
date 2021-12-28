6df02b3fd599777e80eb60a6eff17f1cfc498272
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-12-28 17:32:14 -0300

    Update ft_printf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -1,41 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/14 19:17:35 by acesar-l          #+#    #+#             */
+/*   Updated: 2021/12/28 21:10:31 by acesar-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int ft_printf(const char *str, ...)
+static int	ft_check_arguments(va_list arg, char c)
 {
-	va_list argptr;
-	int i;
-	int bytes_writed;
+	int	bytes;
+
+	bytes = 0;
+	if (c == '%')
+		bytes += ft_argument_percent();
+	if (c == 'c')
+		bytes += ft_argument_c(va_arg(arg, int));
+	if (c == 's')
+		bytes += ft_argument_s(va_arg(arg, char *));
+	if (c == 'p')
+		bytes += ft_argument_p(va_arg(arg, unsigned long long int));
+	if (c == 'd' || c == 'i')
+		bytes += ft_arguments_d_i(va_arg(arg, int));
+	if (c == 'u')
+		bytes += ft_argument_u(va_arg(arg, int));
+	if (c == 'x' || c == 'X')
+		bytes += ft_arguments_x(va_arg(arg, unsigned int), c);
+	return (bytes);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list		arg;
+	int			i;
+	int			bytes;
+	char		c;
 
 	i = 0;
-	bytes_writed = 0;
-	va_start(argptr, str);
+	bytes = 0;
+	va_start(arg, str);
 	while (str[i])
 	{
+		c = str[i + 1];
 		if (str[i] == '%')
-        {
-			if (str[i + 1] == '%')
-				bytes_writed += ft_argument_percent();
-			if (str[i + 1] == 'c')
-				bytes_writed += ft_argument_c(va_arg(argptr, int));
-			if (str[i + 1] == 's')
-				bytes_writed += ft_argument_s(va_arg(argptr, char *));
-			if (str[i + 1] == 'p')
-				bytes_writed += ft_argument_p(va_arg(argptr, unsigned long long int));
-			if (str[i + 1] == 'd' || str[i + 1] == 'i')
-				bytes_writed += ft_arguments_d_i(va_arg(argptr, int));
-			if (str[i + 1] == 'u')
-				bytes_writed += ft_argument_u(va_arg(argptr, int));
-			if (str[i + 1] == 'x' || str[i + 1] == 'X')
-				bytes_writed += ft_arguments_x(va_arg(argptr, unsigned int), str[i + 1]);
+		{
+			bytes += ft_check_arguments(arg, c);
 			i++;
 		}
 		else
-		{
-			ft_putchar_fd(str[i], 1);
-			bytes_writed++;
-		}
+			bytes += ft_putchar_bw(str[i]);
 		i++;
 	}
-	va_end(argptr);
-	return (bytes_writed);
+	va_end(arg);
+	return (bytes);
 }
