081f37e7dc37304fb5784d25f9672bc663e7ae67
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-18 13:48:59 -0300

    Update ft_arguments_x.c

--- printf/ft_arguments_x.c
+++ printf/ft_arguments_x.c
@@ -1,50 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_arguments_x.c                                   :+:      :+:    :+:   */
+/*   ft_arguments_x_X.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:12:00 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/12/14 19:12:02 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/12/18 13:52:02 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_argument_size(unsigned int nbr)
-{
-	int	size;
-
-	size = 0;
-	while (nbr > 0)
-	{
-		nbr /= 16;
-		size++;
-	}
-	return (size);
-}
-
-int	ft_arguments_x(unsigned int nbr, char type)
+int	ft_arguments_x_X(unsigned int nbr, char type)
 {
 	char	*num;
-	int	size;
+	int	bytes;
 	int	i;
 
 	i = 0;
 	if (!nbr)
-		return (ft_putchar_bw('0'));
-	num = (char *)malloc((ft_argument_size(nbr) + 1) * sizeof(char));
+		return (write(1, "0", sizeof(char) * 1));
+	num = (char *)malloc((ft_hex_length(nbr) + 1) * sizeof(char));
 	while (nbr)
 	{
-		num[i] = ft_hexa_converter(nbr % 16, type);
+		num[i] = ft_hex_converter_to_decimal(nbr % 16, type);
 		nbr /= 16;
 		i++;
 	}
 	num[i] = '\0';
-	ft_revstr(num);
+	ft_revert_str(num);
 	ft_putstr_fd(num, 1);
-	size = ft_strlen(num);
+	bytes = ft_strlen(num);
 	ft_free_ptr(&num);
-	return (size);
+	return (bytes);
 }
