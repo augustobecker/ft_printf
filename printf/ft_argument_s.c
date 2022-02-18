bb88430df199bdd784189b94ad99467e3a4ff155
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-18 14:01:37 -0300

    Update ft_argument_s.c

--- printf/ft_argument_s.c
+++ printf/ft_argument_s.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:04:07 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/12/14 19:04:32 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/12/18 14:04:32 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 int	ft_argument_s(char *str)
 {
 	if (str == NULL)
-	{
-		ft_putstr_fd("(null)", 1);
-		return (6);
-	}
+		write(1, "(null)", sizeof(char) * 6);
 	ft_putstr_fd(str, 1);
 	return (ft_strlen(str));
 }
