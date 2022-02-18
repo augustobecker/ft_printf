e7607ed5651b4520feb3ce025ba5bdf3811d30fd
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-18 13:11:10 -0300

    Update ft_printf.h

--- library/ft_printf.h
+++ library/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:28:57 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/12/14 19:30:14 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/02/18 13:01:14 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@
 int   ft_printf(const char *str, ...);
 int   ft_argument_c(char c);
 int   ft_arguments_d_i(int c);
-int   ft_argument_p(unsigned long long int address);
+int   ft_argument_p(unsigned long nbr);
 int   ft_argument_percent(void);
 int   ft_argument_s(char *str);
 int   ft_argument_u(unsigned int nbr);
-int   ft_arguments_x(unsigned int nbr, char type);
+int   ft_arguments_x_X(unsigned int nbr, char type);
 int   ft_is_argument(char a);
-int   ft_nbrlen(long int nbr);
+int   ft_decimal_length(long int nbr);
 void  ft_free_ptr(char **ptr);
-int   ft_putchar_bw(char c);
-char  *ft_revstr(char *str);
-char  ft_hexa_converter(char digit, char type);
+char  *ft_revert_str(char *str);
+char  ft_hex_converter_to_decimal(char digit, char type);
+int   ft_hex_in_decimal_length(unsigned int nbr);
 
 #endif
