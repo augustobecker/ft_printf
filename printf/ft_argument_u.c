2a14ecf391041d1b0ec0c90678d1a0097f97d7e6
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-19 12:09:02 -0300

    Update ft_argument_u.c

--- printf/ft_argument_u.c
+++ printf/ft_argument_u.c
@@ -6,25 +6,25 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:04:59 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/12/18 15:05:01 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/02/18 23:54:33 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_put_unsigned_nbr(unsigned int nb)
+static void	ft_put_unsigned_nbr(unsigned int nbr)
 {
-	if (nb < 10)
-		ft_putchar_fd(nb + 48, 1);
+	if (nbr < 10)
+		ft_putchar_fd(nbr + '0', 1);
 	else
 	{
-		ft_put_unsigned_nbr(nb / 10);
-		ft_put_unsigned_nbr(nb % 10);
+		ft_put_unsigned_nbr(nbr / 10);
+		ft_put_unsigned_nbr(nbr % 10);
 	}	
 }
 
 int	ft_argument_u(unsigned int nbr)
 {
 	ft_put_unsigned_nbr(nbr);
-	return (ft_decimal_lentgh(nbr));
+	return (ft_decimal_length(nbr));
 }
