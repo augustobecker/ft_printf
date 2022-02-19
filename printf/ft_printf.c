fce9e1ba15bef4ab1c4e886400ff02aa5ef828ae
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-19 10:18:41 -0300

    Update ft_printf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -30,7 +30,7 @@ static int	ft_check_arguments(va_list arg, char c)
 	if (c == 'u')
 		bytes += ft_argument_u(va_arg(arg, int));
 	if (c == 'x' || c == 'X')
-		bytes += ft_arguments_x_X(va_arg(arg, unsigned int), c);
+		bytes += ft_arguments_x(va_arg(arg, unsigned int), c);
 	return (bytes);
 }
 
