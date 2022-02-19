474ddb39167a5475a2ac45e6fd1550e08c4ddbc9
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-02-19 12:09:51 -0300

    Update ft_arguments_d_i.c

--- printf/ft_arguments_d_i.c
+++ printf/ft_arguments_d_i.c
@@ -6,33 +6,33 @@
 /*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 19:05:57 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/02/18 13:45:58 by acesar-l         ###   ########.fr       */
+/*   Updated: 2022/02/18 20:33:13 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void	ft_putnbr(int nb)
+static void	ft_putnbr(int nbr)
 {
-	int	soma;
+	int	sum;
 
-	soma = 0;
-	if (nb < 0)
+	sum = 0;
+	if (nbr < 0)
 	{
 		write(1, "-", 1);
-		nb *= -1;
-		if (nb == -2147483648)
+		nbr *= -1;
+		if (nbr == -2147483648)
 		{
-			nb--;
-			soma = 1;
+			nbr--;
+			sum = 1;
 		}
 	}
-	if (nb < 10)
-		ft_putchar_fd(nb + 48 + soma, 1);
+	if (nbr < 10)
+		ft_putchar_fd(nbr + 48 + sum, 1);
 	else
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10 + soma);
+		ft_putnbrr(nbr / 10);
+		ft_putnbrr(nbr % 10 + sum);
 	}	
 }
 
